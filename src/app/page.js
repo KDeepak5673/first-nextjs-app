7727414a0c9c9a2332f27aa21e9a73dd838af289
KDeepak5673 <deepakchandrashekhar0309@gmail.com>
2025-06-05 01:01:47 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>src/app/page.js</code>.
+            Hello I'am Deepak kumar <code>A cloud developer</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
